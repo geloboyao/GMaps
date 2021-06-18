6acece69e5af4ada46acab57ecaf89cf68546262
Angelo Alimboyao <angelo.alimboyao@gmail.com>
2021-06-18 23:41:10 +0800

    init

---
+++ GoogleMaps/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace GoogleMaps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(12, 65);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(589, 373);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.Load += new System.EventHandler(this.map_Load);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(12, 39);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(199, 20);
+            this.txtFrom.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(422, 39);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(217, 39);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(199, 20);
+            this.txtTo.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 450);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtTo;
+    }
+}
+
---
+++ GoogleMaps/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleMaps
+{
+    public partial class Form1 : Form
+    {
+        // initialize the overlays
+        GMap.NET.WindowsForms.GMapOverlay fromMarkers = new GMap.NET.WindowsForms.GMapOverlay("fromMarkers");
+        GMap.NET.WindowsForms.GMapOverlay toMarkers = new GMap.NET.WindowsForms.GMapOverlay("toMarkers");
+        GMap.NET.WindowsForms.GMapOverlay routes = new GMap.NET.WindowsForms.GMapOverlay("routes");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void map_Load(object sender, EventArgs e)
+        {
+            // get your api key, refer at https://www.youtube.com/watch?v=eXJ1qkTMLw8
+            GMap.NET.MapProviders.GMapProviders.GoogleMap.ApiKey = "<YOUR KEY HERE>";
+
+            // initialize primary coordinates. you can set your own.
+            double lat = 14.588493;
+            double lng = 121.0427838;
+
+            // initialize map behavior
+            map.ShowCenter = false;
+            map.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            map.DragButton = MouseButtons.Left;
+            map.Position = new GMap.NET.PointLatLng(lat, lng);
+            map.MinZoom = 11;
+            map.MaxZoom = 17;
+            map.Zoom = 11;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // remove existing overlays
+            map.Overlays.Remove(fromMarkers);
+            map.Overlays.Remove(toMarkers);
+            map.Overlays.Remove(routes);
+
+            if (fromMarkers.Markers.Count > 0)
+            {
+                fromMarkers.Markers.RemoveAt(0);
+            }
+
+            if (toMarkers.Markers.Count > 0)
+            {
+                toMarkers.Markers.RemoveAt(0);
+            }
+
+            if (routes.Routes.Count > 0)
+            {
+                routes.Routes.RemoveAt(0);
+            }
+
+            map.Refresh();
+
+            // get the addresses
+            String from = txtFrom.Text.Trim();
+            String to = txtTo.Text.Trim();
+            Console.WriteLine(from);
+            Console.WriteLine(to);
+
+            // initialize from at to coordinates
+            double fromlat;
+            double fromlng;
+            double tolat;
+            double tolng;
+
+            // if addresses filled up
+            if (!from.Equals("") && !to.Equals(""))
+            {
+                GMap.NET.GeoCoderStatusCode statusCodeFrom;
+                GMap.NET.GeoCoderStatusCode statusCodeTo;
+
+                // get the geocode of the addresses
+                var fromlatlng = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetPoint(from, out statusCodeFrom);
+                var tolatlng = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetPoint(to, out statusCodeTo);
+
+                // if geocoder status ok, process everything
+                if (statusCodeFrom == GMap.NET.GeoCoderStatusCode.OK && statusCodeTo == GMap.NET.GeoCoderStatusCode.OK)
+                {
+                    // get the from latlng
+                    double.TryParse(fromlatlng?.Lat.ToString(), out fromlat);
+                    double.TryParse(fromlatlng?.Lng.ToString(), out fromlng);
+                    var fromPosition = new GMap.NET.PointLatLng(fromlat, fromlng);
+
+                    // initialize from marker
+                    var fromMarker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(fromPosition, GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red);
+                    fromMarkers.Markers.Add(fromMarker);
+                    map.Overlays.Add(fromMarkers);
+
+                    // get the to latlng
+                    double.TryParse(tolatlng?.Lat.ToString(), out tolat);
+                    double.TryParse(tolatlng?.Lng.ToString(), out tolng);
+                    var toPosition = new GMap.NET.PointLatLng(tolat, tolng);
+
+                    // initialize the to marker
+                    var toMarker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(toPosition, GMap.NET.WindowsForms.Markers.GMarkerGoogleType.red);
+                    toMarkers.Markers.Add(toMarker);
+                    map.Overlays.Add(toMarkers);
+
+                    // initialize route
+                    var route = GMap.NET.MapProviders.GMapProviders.GoogleMap.GetRoute(fromPosition, toPosition, false, false, 11);
+
+                    var r = new GMap.NET.WindowsForms.GMapRoute(route.Points, "My Route");
+                    routes.Routes.Add(r);
+                    map.Overlays.Add(routes);
+
+                    // set the zoom and coordinates
+                    map.Position = new GMap.NET.PointLatLng(fromlat, fromlng);
+                    map.Zoom = 14;
+
+                    // get the distance
+                    var distance = route.Distance;
+
+                    // display the distance on console
+                    Console.WriteLine(distance);
+                }
+            }
+        }
+    }
+}
